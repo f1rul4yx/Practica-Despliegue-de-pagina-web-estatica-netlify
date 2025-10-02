6de958f75c73eb318767292c8c72f17042df5c55
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-10-02 23:20:55 +0200

    Actualización automática

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,30 @@
-const swconf = { cacheName: 'chirpy-1759439101',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1759440053',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
