bb82957c1196a2b5c60e1c6d241928d07b9fe836
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-10-03 12:42:19 +0200

    Actualización automática

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759488117',resources: [
+    cacheName: 'chirpy-1759488137',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
